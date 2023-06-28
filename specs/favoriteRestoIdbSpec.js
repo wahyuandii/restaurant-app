6953f704ec7a139df3ab9eaa7868a3d81a578638
Azamnst <43640396+Azamnst@users.noreply.github.com>
2023-06-28 21:39:01 +0700

    Update

--- specs/favoriteRestoIdbSpec.js
+++ specs/favoriteRestoIdbSpec.js
@@ -1,21 +1,12 @@
-const assert = require('assert');
+import { itActsAsFavoriteRestoModel } from './contract/favoriteRestoContract';
+import FavoriteRestoIdb from "../src/scripts/data/favoriterestoidb";
 
-Feature('Favorite Resto Idb Contract Test');
+describe('Favorite Resto Idb Contract Test Implementation', () => {
+  afterEach(async () => {
+    (await FavoriteRestoIdb.getAllRestos()).forEach(async (resto) => {
+      await FavoriteRestoIdb.deleteResto(resto.id);
+    });
+  });
 
-Scenario('saving a resto', async ({ I }) => {
-  I.amOnPage('/');
-
-  I.seeElement('.list_item_title a');
-  const firstResto = locate('.list_item_title a').first();
-  const firstRestoTitle = await I.grabTextFrom(firstResto);
-  I.click(firstResto);
-
-  I.seeElement('#likeButton');
-  I.click('#likeButton');
-
-  I.amOnPage('#/favorite');
-  I.seeElement('.list_item');
-  const likedRestoTitle = await I.grabTextFrom('.list_item_title a');
-
-  assert.strictEqual(firstRestoTitle, likedRestoTitle);
+  itActsAsFavoriteRestoModel(FavoriteRestoIdb);
 });
